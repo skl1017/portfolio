e9559fd0bc1e4460036f0d8f84fa70dfee86cc4f
skl1017 <loan.riyanto.pro@gmail.com>
2023-03-24 15:32:38 +0100

    js
    js + anchors

--- js/index.js
+++ js/index.js
@@ -35,7 +35,18 @@ window.onload = (event) =>{
         menu.classList.toggle('show');
     })
 
-
+    //add show-loader then after 1s remove it with hide-loader
+    const loader = document.querySelector('.loader');
+    loader.classList.add('show-loader');
+    setTimeout(() => {
+        loader.classList.remove('show-loader');
+        loader.classList.add('hide-loader');
+    }, 1000);
+    //after 1 second, put it in display none
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 2000);
+    
 
     window.onscroll = (event) =>{
         var y = window.scrollY;
@@ -57,7 +68,32 @@ window.onload = (event) =>{
         }
     }
 
+    const observer = new IntersectionObserver(callback); // création d'un observer avec l'API Intersection Observer qui va appeler la fonction callback 
+                                                      //à chaque changement de visibilité d'un élément
+
+function callback(entries){ // va pour chaque entrée, si isIntersecting est true (qu'il apparait à l'écran), rajouter la classe show, et donc 
+                            //lancer l'animation (qu'on peut trouver dans le css)
+    entries.forEach((entry) => {
+        if (entry.isIntersecting){
+          console.log(entry)
+          entry.target.classList.add('article-show');
+        }
+      })
+}
+
+const elements = document.querySelectorAll('article'); //selectionne ce que l'on veut animer 
+
+elements.forEach((element) => element.classList.add('article-hidden')); // cache les éléments en ajoutant la classe hidden, afin de pouvoir 
+                                                                //apres les afficher avec une animation
+
+elements.forEach((element) => {console.log(element);
+  observer.observe(element);
+   // va observer chaque élément et va appeler 'observer' à chaque fois 
+});
+    
     
     
 }
+
+
 
